d9547c7091d01ffa7e98a7d76acf22749d973cb5
Filipe Correia <filipe.lp.correia@gmail.com>
2020-05-24 23:50:33 +0100

    ci: testing out coverage reporting in single tests

--- e2e/example.test.ts
+++ e2e/example.test.ts
@@ -1,12 +1,16 @@
 import 'expect-puppeteer'
+const pti = require('puppeteer-to-istanbul')
 
 describe('Google', () => {
   beforeAll(async () => {
+    page.coverage.startJSCoverage()
     await page.goto((global as any).URL)
   })
 
   it('should display "inbox" text on page', async () => {
     await expect(page).toMatch('Inbox')
     await (global as any).snapshot('Inbox')
+    const jsCoverage = await page.coverage.stopJSCoverage()
+    pti.write(jsCoverage)
   })
 })
